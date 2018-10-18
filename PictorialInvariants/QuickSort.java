f67071205ae7cc9c438d2ef36dce0b979f1edf16
y <dangolpr@grinnell.edu>
2018-10-18 16:58:11 -0500

    added comments for quicksort

--- PictorialInvariants/QuickSort.java
+++ PictorialInvariants/QuickSort.java
@@ -1,7 +1,8 @@
 package loopInvariants;
 
+/** This program sorts an array of numbers using the quick sort method*/
+
 public class QuickSort {
-	
 	public static int partition (int a[], int left, int right) {
 		int l_spot= left +1;
 		int pivot = a[left];
@@ -42,6 +43,7 @@ public class QuickSort {
 	 * 			n : length of array
 	 */
 	public static void quicksort (int a[], int n) {
+		if (a.length != 0) 
 		quicksortKernel(a, 0, n-1);
 	}
 	
@@ -49,6 +51,7 @@ public class QuickSort {
 	 * @param : a[] : array 
 	 * 		   left  : left index of the array
 	 *         right: the right most index of the array 
+	 * @return: 0 if the array a[] is sorted
 	 */
 	public static int quicksortKernel(int a[], int left, int right) {
 		//stores the value of the new position of pivot in index
@@ -74,13 +77,65 @@ public class QuickSort {
 	
 	
 	public static void main (String args[]) {
-		int [] arr = {70, 80, 30, 90, 40, 60, 50, 10};
-		quicksort(arr, arr.length);
-		System.out.println("Sorted array: ");
+		
+		//testing in main 
+	
+		
+		//testing with a sorted array 
+		int [] arr1 = {10, 30, 40, 50, 60, 70, 80, 90};
+		
+		//testing with a random set of integers
+		int [] arr2 = {60, 70, 100, 5, 3, 4, 1, 9};
+		
+		//testing with an empty array 
+		int [] arr3 = {};
+		
+		//testing with the minimum value at the start of the array
+		int [] arr4 = {1, 70, 100, 5, 3, 4, 2, 9};
+		
+		//testing with the minimum value at the end of an array
+		int [] arr5 = {60, 70, 100, 5, 3, 40, 2, 1};
+		
+		
+		quicksort(arr1, arr1.length);
+		quicksort(arr2, arr2.length);
+		quicksort(arr3, arr3.length);
+		quicksort(arr4, arr4.length);
+		quicksort(arr5, arr5.length);
+		
+		System.out.println("Sorted array : ");
 		System.out.print("{");
-		for (int i= 0; i < arr.length-1; i++) {
-			System.out.print(arr[i] + ", ");
+		for (int i= 0; i < arr1.length-1; i++) {
+			System.out.print(arr1[i] + ", ") ;
 		}
-		System.out.print(arr[arr.length-1] + "} ");
+		System.out.print(arr1[arr1.length-1] + "} \n ");
+		
+		System.out.println("Sorted array (2): ");
+		System.out.print("{");
+		for (int i= 0; i < arr2.length-1; i++) {
+			System.out.print(arr2[i] + ", ");
+		}
+		System.out.print(arr1[arr2.length-1] + "} ");
+		
+		System.out.println("\nSorted array (3): ");
+		for (int i= 0; i < arr3.length; i++) {
+			System.out.print(arr3[i] + ", ");
+		}
+		//System.out.print(arr3[arr3.length-1] + "} ");
+		
+		System.out.println("\n Sorted array (4): ");
+		System.out.print("{");
+		for (int i= 0; i < arr4.length-1; i++) {
+			System.out.print(arr4[i] + ", ");
+		}
+		System.out.print(arr4[arr4.length-1] + "} ");
+		
+		System.out.println("\nSorted array(5): ");
+		System.out.print("{");
+		for (int i= 0; i < arr5.length-1; i++) {
+			System.out.print(arr5[i] + ", ");
+		}
+		System.out.print(arr5[arr5.length-1] + "} ");
 	}
 }
+
